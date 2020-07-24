292210c7575504e9d3edfedad7d5b7c01680cbe4
kapil gupta <kapil@workifyd.com>
2020-07-24 16:17:06 +0530

    Inital commit in Spring boot dependency injection examples

---
+++ src/main/java/com/springframework/jkdi/JkDiApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.jkdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/jkdi/JkDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.jkdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
