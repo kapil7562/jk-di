20ac4bd6a5d3fbf17f7d2842beadb72bb33c5718
kapil gupta <kapil@workifyd.com>
2020-07-24 16:45:19 +0530

    Configure MyContrller Bean using ConfigurableApplicationContext to show Dependency injection

--- src/main/java/com/springframework/jkdi/JkDiApplication.java
+++ src/main/java/com/springframework/jkdi/JkDiApplication.java
@@ -2,12 +2,19 @@ package com.springframework.jkdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.springframework.controllers.MyController;
+
+@ComponentScan("com.springframework.controllers")
 @SpringBootApplication
 public class JkDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JkDiApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(JkDiApplication.class, args);
+		MyController myCotroller = ctx.getBean(MyController.class);
+		System.out.println(myCotroller.sayHello());
 	}
 
 }
